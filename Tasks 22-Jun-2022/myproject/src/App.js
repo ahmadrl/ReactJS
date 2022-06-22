3e8537e7089e36f286fd69ee46f9456e774b8d61
AhmadKhalaf <65103614+ahmadrl@users.noreply.github.com>
2022-06-22 15:47:56 +0200

    Add files via upload

---
+++ Tasks 22-Jun-2022/myproject/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import SortChars from './components/SortChars';
+import Map from './components/Map';
+import SearchFilter from './components/SearchFilter';
+
+
+function App() {
+  return (
+    <>
+    <Map />
+    <SortChars />
+    <SearchFilter />
+    </>
+
+  );
+}
+
+export default App;
