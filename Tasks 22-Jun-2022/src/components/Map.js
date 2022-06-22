ab5dcdcb04b096bcd8d999b9a5522d5142fb9732
AhmadKhalaf <65103614+ahmadrl@users.noreply.github.com>
2022-06-22 16:24:24 +0200

    Add files via upload

---
+++ Tasks 22-Jun-2022/src/components/Map.js
@@ -0,0 +1,25 @@
+function Map()
+ {
+    const Fruits = [
+        {id: 1 , name: "Apple",  color: "Yellow"},
+        {id: 2 , name: "Strawberry", color: "Red"},
+        {id: 3 , name: "Kiwi", color: "Green"}
+      ]; 
+      let mapping = Fruits.map(fruit =>
+      <table id={fruit.id}>
+        <tr>{fruit.name}/{fruit.color}</tr>
+      </table>); 
+
+    return (
+
+      <div>
+        {
+       {mapping}
+      }
+      </div>
+    );
+  };
+  export default Map;
+
+
+
